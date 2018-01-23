c69e7dbca70b7934700162f40c7cd7169f9efae9
Ruling Fong <rulingfong@outlook.com>
2018-01-23 13:33:37 +0800

    存档

--- init.el
+++ init.el
@@ -32,6 +32,13 @@
 		;; 自己加的插件
 		reveal-in-osx-finder
 		org-pomodoro
+		evil
+		highlight-parentheses
+		helm
+		window-numbering
+		sr-speedbar
+		tabbar
+		;;ecb
 		) "Default packages")
 
  (setq package-selected-packages my/packages)
@@ -61,13 +68,17 @@
 ;; 加载目录
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
+;; 主题
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+;;(setq molokai-theme-kit t)
+
 ;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
 (tool-bar-mode -1)
 
 (package-initialize)
 
 ;; 设置字体大小
-(set-face-attribute 'default nil :height 160)
+(set-face-attribute 'default nil :height 180)
 
 
 ;; 关闭文件滑动控件
@@ -103,9 +114,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c3c0a3702e1d6c0373a0f6a557788dfd49ec9e66e753fb24493579859c8e95ab" default)))
  '(package-selected-packages
    (quote
-    (helm-gtags helm window-numbering org-pomodoro js3-mode company hungry-delete swiper counsel smartparens js2-mode nodejs-repl exec-path-from-shell monokai-theme)))
+    (tabbar sr-speedbar ecb window-number evil helm-gtags helm window-numbering org-pomodoro js3-mode company hungry-delete swiper counsel smartparens js2-mode nodejs-repl exec-path-from-shell monokai-theme)))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -183,4 +197,129 @@
   (lambda ()  
     (highlight-parentheses-mode t)))  
 (global-highlight-parentheses-mode t)
+
+;; evil-mode
+(require 'evil)
+(evil-mode 1)
+
+;; 切换窗口
+(require 'window-numbering)
+(window-numbering-mode 1)
+
+;; Collection of Emacs Development Environment Tools的缩写， 意为"Emacs开发环境工具集"
+;;; CEDET
+; (require 'cedet)
+; (semantic-mode t)
+; (global-ede-mode t)
+
+; ;;;; Include settings
+; (require 'semantic/bovine/gcc)
+; (require 'semantic/bovine/c)
+
+; (defconst cedet-user-include-dirs
+;   (list ".." "../include" "../inc" "../common" "../public" "."
+;         "../.." "../../include" "../../inc" "../../common" "../../public"))
+
+; (setq cedet-sys-include-dirs (list
+;                               "/usr/include"
+;                               "/usr/include/bits"
+;                               "/usr/include/glib-2.0"
+;                               "/usr/include/gnu"
+;                               "/usr/include/gtk-2.0"
+;                               "/usr/include/gtk-2.0/gdk-pixbuf"
+;                               "/usr/include/gtk-2.0/gtk"
+;                               "/usr/local/include"
+;                               "/usr/local/include"))
+
+; (let ((include-dirs cedet-user-include-dirs))
+;   (setq include-dirs (append include-dirs cedet-sys-include-dirs))
+;   (mapc (lambda (dir)
+;           (semantic-add-system-include dir 'c++-mode)
+;           (semantic-add-system-include dir 'c-mode))
+;         include-dirs))
+
+; (setq semantic-c-dependency-system-include-path "/usr/include/")
+
+;; ecb
+;;(require 'ecb)
+; disable the tip-of-the-day window, it is annoying and most importantly, it will hang my emacs
+;;(setq ecb-tip-of-the-day nil)
+
+; defualt value is 'mouse-2--C-mouse-2, so that I can't open file by clicking.
+; Change to 'mouse-1--C-mouse-1 to enable click-to-open.
+;;(setq ecb-primary-secondary-mouse-buttons 'mouse-1--C-mouse-1)
+
+;; sr-speedbar
+(require 'sr-speedbar)
+(setq sr-speedbar-right-side nil)
+(setq sr-speedbar-width 25)
+(setq dframe-update-speed t)
+(global-set-key (kbd "<f3>") (lambda()
+          (interactive)
+          (sr-speedbar-toggle)))
+
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+;; tabbar
+(require 'tabbar)  
+(tabbar-mode 1)
+(set-face-attribute
+ 'tabbar-default nil
+ :background "gray20"
+ :foreground "gray20"
+ :box '(:line-width 1 :color "gray20" :style nil))
+(set-face-attribute
+ 'tabbar-unselected nil
+ :background "gray30"
+ :foreground "white"
+ :box '(:line-width 5 :color "gray30" :style nil))
+(set-face-attribute
+ 'tabbar-selected nil
+ :background "gray75"
+ :foreground "black"
+ :box '(:line-width 5 :color "gray75" :style nil))
+(set-face-attribute
+ 'tabbar-highlight nil
+ :background "white"
+ :foreground "black"
+ :underline nil
+ :box '(:line-width 5 :color "white" :style nil))
+(set-face-attribute
+ 'tabbar-button nil
+ :box '(:line-width 1 :color "gray20" :style nil))
+(set-face-attribute
+ 'tabbar-separator nil
+ :background "gray20"
+ :height 0.6)
+ (defun tabbar-buffer-groups ()
+   (list
+    (cond
+     ((or (get-buffer-process (current-buffer))
+          ;; Check if the major mode derives from `comint-mode' or `compilation-mode'.
+          (tabbar-buffer-mode-derived-p
+           major-mode '(comint-mode compilation-mode)))
+      "Process"
+      )
+     ((and (string-equal "*" (substring (buffer-name) 0 1)) (string-equal "*nrepl*" (buffer-name)))
+      "Common"
+      )
+     ((memq major-mode
+     		 '(python-mode))
+      "python"
+      )
+     (t
+      ;; Return `mode-name' if not blank, `major-mode' otherwise.
+      (if (and (stringp mode-name)
+               ;; Take care of preserving the match-data because this
+               ;; function is called when updating the header line.
+               (save-match-data (string-match "[^ ]" mode-name)))
+          mode-name
+        (symbol-name major-mode))
+      ))))
 
